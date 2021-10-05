4ad54a8d7a375f2f9d4e22f29abdc629ec62a934
Samuel Medina <smdoomed@gmail.com>
2021-10-05 11:27:03 -0500

    iconos en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,9 +22,18 @@ const Footer = ({ setAddQuote, setView }) => {
     return (
         <footer className="footer footerMobile">
             <ul>
-                <li className="footer-item mobile" onClick={handleClickOnHome}> <i class="fas fa-home"></i> </li>
-                <li className="footer-item mobile" onClick={handleClickOnQuotes}> Frases </li>
-                <li className="footer-item mobile" onClick={handleClickOnAuthors}> Autores </li>
+                <li className="footer-item mobile" onClick={handleClickOnHome}>
+                    <i class="fas fa-home fa-2x"></i>
+                    <span> Home </span>
+                </li>
+                <li className="footer-item mobile" onClick={handleClickOnQuotes}>
+                    <i class="fas fa-quote-right fa-2x"></i>
+                    <span> Frases </span>
+                </li>
+                <li className="footer-item mobile" onClick={handleClickOnAuthors}>
+                <i class="fas fa-users fa-2x"></i>
+                    <span> Autores </span>
+                </li>
                 <li className="footer-item mobile plusButton" onClick={handleAdd}><b> + </b></li>
 
                 <li className="footer-item desktop"> Un producto de <a href="./"> samuel-mc </a>  </li>
